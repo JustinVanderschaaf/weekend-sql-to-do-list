dbce04cbb49fc6035c41ca6af440ec5a57e4eb04
Justin Vanderschaaf <Vanderschaaf.usmc@gmail.com>
2022-01-06 22:07:32 -0600

    created SQL database

---
+++ database.sql
@@ -0,0 +1,9 @@
+CREATE TABLE "tasks"(
+"id" serial, "task" varchar(255), "completed" BOOLEAN DEFAULT FALSE);
+
+INSERT INTO "tasks" 
+	(task, completed) 
+	VALUES(
+	'dishes','FALSE'),
+   ('shovel','TRUE'),
+ ( 'garbage','FALSE')
